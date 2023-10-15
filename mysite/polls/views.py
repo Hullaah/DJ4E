fa9427baf67fe1c043e2d5ba8ae1825efe1dc3d1
Hullaah <umaradelo1.247@gmail.com>
2023-10-15 17:22:54 +0100

    Edited some files

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You are at the polls index.")
+    return HttpResponse("Hello, world. 87383af6 is the polls index.")
