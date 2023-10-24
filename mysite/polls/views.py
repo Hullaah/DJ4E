ba07d3af16e64d21f1bd0f2f3cdf7e96177a9c91
Hullaah <umaradelo1.247@gmail.com>
2023-10-24 17:00:11 +0100

    Modified the tutorial 3 for the dj4e assignment

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -27,3 +27,7 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+
+def owner(request):
+    return HttpResponse("Hello, world. 87383af6 is the polls index.")
