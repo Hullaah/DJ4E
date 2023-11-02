0ffe3eabe05cbc317e9f776300827ba70acbfc41
Hullaah <umaradelo1.247@gmail.com>
2023-11-02 17:34:20 +0100

    Corrected the view for hello page to display 'view coun' instead of 'num_visits'

--- mysite/hello/views.py
+++ mysite/hello/views.py
@@ -7,6 +7,6 @@ def hello_view(request: HttpRequest):
     request.session["num_visits"] = num_visits
     if num_visits > 4:
         del request.session["num_visits"]
-    resp = HttpResponse('num_visits=' + str(num_visits))
+    resp = HttpResponse('view count=' + str(num_visits))
     resp.set_cookie('dj4e_cookie', '87383af6', max_age=1000)
     return resp
